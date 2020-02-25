9bb7207839fa7623ca6dc1ffcdae516177beecc7
Grochowskij <59901129+Grochowskij@users.noreply.github.com>
2020-02-25 11:38:32 -0600

    Update Cmpsc390Project.java

--- Netbeans Project/src/cmpsc390project/Cmpsc390Project.java
+++ Netbeans Project/src/cmpsc390project/Cmpsc390Project.java
@@ -13,8 +13,6 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Callback;
 
