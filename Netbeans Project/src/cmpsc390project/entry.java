0aa0a6d76e316d4cfa49fb74eb2616f145349cd6
Gabby <gabby@users.noreply.github.com>
2020-04-07 13:55:24 -0500

    Create entry.java

---
+++ Netbeans Project/src/cmpsc390project/entry.java
@@ -0,0 +1,29 @@
+package cmpsc390project;
+
+public class entry {
+	private String name;
+	private int set;
+	private int rep;
+	private int temp;
+	private int amount;
+	
+	public entry(String a, int b, int c, int d){
+		name = a;
+		set = b;
+		rep = c;
+		temp = d;
+		amount = set * rep;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public int getAmount() {
+		return amount;
+	}
+	
+	public void setAmount(int add) {
+		amount = add;
+	}
+}
