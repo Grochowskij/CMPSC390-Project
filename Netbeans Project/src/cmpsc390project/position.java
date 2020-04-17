d7794ef938d332e961658977dc4bc849f55b6890
Grochowskij <59901129+Grochowskij@users.noreply.github.com>
2020-04-17 14:02:09 -0500

    Update position.java

--- Netbeans Project/src/cmpsc390project/position.java
+++ Netbeans Project/src/cmpsc390project/position.java
@@ -20,48 +20,7 @@ public class position {
     	entry second = new entry("none2",0,0,0);
     	entry third = new entry("none3",0,0,0);
     	
-    	try {
-    		File data = new File("Workoutinput.txt");
-			BufferedReader reader = new BufferedReader(new FileReader(data));
-			Scanner scan = new Scanner(reader);
-			
-			while (scan.hasNext())
-			{entry temp = new entry(scan.next(),scan.nextInt(),scan.nextInt(),scan.nextInt());
-			
-			if (temp.getName().contentEquals(first.getName()))
-			{first.setAmount(first.getAmount() + temp.getAmount());}
-			else if (temp.getName().contentEquals(second.getName()))
-			{second.setAmount(second.getAmount() + temp.getAmount());
-			if (second.getAmount() > first.getAmount())
-			{temp = first;
-			first = second;
-			second = temp;}}
-			else if (temp.getName().contentEquals(third.getName()))
-			{third.setAmount(third.getAmount() + temp.getAmount());
-			if (third.getAmount() > first.getAmount())
-			{temp = first;
-			first = third;
-			third = second;
-			second = temp;}
-			else if (third.getAmount() > second.getAmount())
-			{temp = second;
-			second = third;
-			third = temp;}}
-			else if (temp.getAmount() > first.getAmount())
-			{third = second;
-			second = first;
-			first = temp;}
-			else if (temp.getAmount() > second.getAmount())
-			{third = second;
-			second = temp;}
-			else if (temp.getAmount() > third.getAmount())
-			{third = temp;}
-			}
-			
-			
-		} catch (FileNotFoundException e) {
-			System.out.println("Error, data missing...");
-		}
+    	
 		
         if (place == 1)
         {excercise = first.getName();
